204a4adc6ff04191a4499fe034a562a610e06154
msoutopico <m.soutopico@gmail.com>
2020-11-09 17:43:54 +0100

    Some folder updates: test > production, and omt_bash > cli_automation

--- mk_rec_omtprj/launcher_Firefly_phase2.sh
+++ mk_rec_omtprj/launcher_Firefly_phase2.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #@Adri, what's the difference between that first line and #!/bin/bash ?
 
-log_dir="/media/data/data/company/PISA_2021/FIELD_TRIAL/99_Automation/01_util/omt_bash/mk_rec_omtprj/logs"
+log_dir="/media/data/data/company/PISA_2021/FIELD_TRIAL/99_Automation/01_util/cli_automation/mk_rec_omtprj/logs"
 now=`date "+%Y%m%d_%H%M%S"`
 log_file="log_${now}.txt"
 
 find $log_dir -type f -empty -delete
 
-workdir="/media/data/data/company/Essence_global/01_PROJECT/05_Firefly_phase2/01_source/test"
+workdir="/media/data/data/company/Essence_global/01_PROJECT/05_Firefly_phase2"
 
-bash /media/data/data/company/PISA_2021/FIELD_TRIAL/99_Automation/01_util/omt_bash/mk_rec_omtprj/mk_rec_omtprj.sh -d $workdir &> $log_dir/$log_file
+bash /media/data/data/company/PISA_2021/FIELD_TRIAL/99_Automation/01_util/cli_automation/mk_rec_omtprj/mk_rec_omtprj.sh -d $workdir &> $log_dir/$log_file
