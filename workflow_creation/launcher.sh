0ba37158ef78eb314559a32e9f92893da9274c8c
msoutopico <m.soutopico@gmail.com>
2021-09-15 23:02:30 +0200

    Changed launcher to loop through root folders of any projects

--- workflow_creation/launcher.sh
+++ workflow_creation/launcher.sh
@@ -4,34 +4,39 @@
 #APP_ROOT="$( dirname "$(readlink -f -- "$0")" )" # always relative parent dir to the script
 APP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 #echo "Getting paths from $APP_ROOT/paths.txt"
-while read -r line; do declare $line; done < "$APP_ROOT/paths.txt"
+while read -r line; do declare $line; done < "$APP_ROOT/paths/config.txt"
+roots=$(cat "$APP_ROOT/paths/roots.txt") 
 
-now=`date "+%Y%m%d_%H%M%S"`
 log_dir=$APP_ROOT/_log
 #log_file="${now}_bash.log"
 log_file="bash_log.txt"
 
-echo $log_dir
-echo $log_file
+for root in $roots; do
 
-# remove emtpy reports
-find $log_dir -type f -empty -delete
+	now=`date "+%Y%m%d_%H%M%S"`
+	config="${root}/02_AUTOMATION/Config/config.xlsx"
+	init_dir="${root}/02_AUTOMATION/Initiation"
 
-echo "Changing directory to the working folder (the app root)" >> $log_dir/$log_file
-cd $cwd
-init=NULL
+	# remove emtpy reports
+	find $log_dir -type f -empty -delete
 
-# grab init bundle path if it exists
-for init_zip_path in $(find $init_dir -maxdepth 1 -name *.zip -type f)
-do
-	init="$init_zip_path"
-	echo "pipenv run python mk_workflows.py -i $init -c $config -m $mapping >> $log_dir/$log_file" >> $log_dir/$log_file
-	#pipenv run python $script -i $init -c $config -m $mapping >> $log_dir/$log_file
-	pipenv run python mk_workflows.py -i $init -c $config -m $mapping >> $log_dir/$log_file
+	echo "${now} @launcher.sh - Changing directory to the working folder ($APP_ROOT)" >> $log_dir/$log_file
+	#cd $cwd
+	cd $APP_ROOT
+	init=NULL
+
+	# grab init bundle path if it exists
+	for init_zip_path in $(find $init_dir -maxdepth 1 -name *.zip -type f)
+	do
+		init="$init_zip_path"
+		#echo "pipenv run python3 mk_workflows.py -i $init -c $config -m $mapping >> $log_dir/$log_file" 
+		echo "${now} @launcher.sh - pipenv run python3 mk_workflows.py -i $init -c $config -m $mapping >> $log_dir/$log_file" >> $log_dir/$log_file
+		#pipenv run python $script -i $init -c $config -m $mapping >> $log_dir/$log_file
+		pipenv run python mk_workflows.py -i $init -c $config -m $mapping >> $log_dir/$log_file
+	done
+	sleep 30 # seconds
 done
 
-# call 
-#pipenv run python $script -i $init -c $config -m $mapping
-#mv $init_zip_path $init_dir/_done # do that in python if init not listed in do_not_archive_bundle
+echo "${now} @launcher.sh - ------------------------------------------------------"
 
 
---
+++ workflow_creation/launcher_paris.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# set up the logs
+#APP_ROOT="$( dirname "$(readlink -f -- "$0")" )" # always relative parent dir to the script
+APP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#echo "Getting paths from $APP_ROOT/paths.txt"
+while read -r line; do declare $line; done < "$APP_ROOT/paths.txt"
+
+now=`date "+%Y%m%d_%H%M%S"`
+log_dir=$APP_ROOT/_log
+#log_file="${now}_bash.log"
+log_file="bash_log.txt"
+
+# remove emtpy reports
+find $log_dir -type f -empty -delete
+
+echo "Changing directory to the working folder (the app root)" >> $log_dir/$log_file
+cd $cwd
+init=NULL
+
+# grab init bundle path if it exists
+for init_zip_path in $(find $init_dir -maxdepth 1 -name *.zip -type f)
+do
+	init="$init_zip_path"
+	echo "pipenv run python mk_workflows.py -i $init -c $config -m $mapping >> $log_dir/$log_file" >> $log_dir/$log_file
+	#pipenv run python $script -i $init -c $config -m $mapping >> $log_dir/$log_file
+	pipenv run python mk_workflows.py -i $init -c $config -m $mapping >> $log_dir/$log_file
+done
+
+# call 
+#pipenv run python $script -i $init -c $config -m $mapping
+#mv $init_zip_path $init_dir/_done # do that in python if init not listed in do_not_archive_bundle
+
+
